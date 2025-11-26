4d1a9ecfb27a6fca79d15574669ae723758a947e
Dario Garcia Vicente <dargarvic@alu.edu.gva.es>
2025-11-26 19:20:11 +0100

    Creacion proyecto Maven Tarea54-DGV25 fix #1

---
+++ Tarea54-DGV25/src/main/java/es/gva/edu/dargarvic/tarea54/dgv25/Tarea54DGV25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.gva.edu.dargarvic.tarea54.dgv25;
+
+/**
+ *
+ * @author Dario
+ */
+public class Tarea54DGV25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
