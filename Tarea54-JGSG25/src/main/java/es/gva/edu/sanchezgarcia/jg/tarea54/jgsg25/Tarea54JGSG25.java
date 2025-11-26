07b9f5084c979f5879e3b14b7a8e4bcecc350044
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2025-11-26 19:20:10 +0100

    Creación proyecto Maven Tarea54-JGSG25 Fix #1

---
+++ Tarea54-JGSG25/src/main/java/es/gva/edu/sanchezgarcia/jg/tarea54/jgsg25/Tarea54JGSG25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.gva.edu.sanchezgarcia.jg.tarea54.jgsg25;
+
+/**
+ *
+ * @author jgaspar
+ */
+public class Tarea54JGSG25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
