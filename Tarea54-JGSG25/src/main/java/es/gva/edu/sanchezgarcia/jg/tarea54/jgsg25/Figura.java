b55158cba24baf78c34ba8fd48f4d8813369e393
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2025-11-26 19:24:08 +0100

    Implementada interfaz Figura.java Fix #2

---
+++ Tarea54-JGSG25/src/main/java/es/gva/edu/sanchezgarcia/jg/tarea54/jgsg25/Figura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package es.gva.edu.sanchezgarcia.jg.tarea54.jgsg25;
+
+/**
+ *
+ * @author jgaspar
+ */
+public interface Figura {
+    public double calcularArea();
+    public double calcularPerimetro();
+            
+    
+}
