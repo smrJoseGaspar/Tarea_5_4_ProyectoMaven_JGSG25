f24923f2bbe6cde3cfa6590be671de8bebb1f65d
Dario Garcia Vicente <dargarvic@alu.edu.gva.es>
2025-11-26 19:24:13 +0100

    Implementada interfaz figura.java fix #2

---
+++ Tarea54-DGV25/src/main/java/es/gva/edu/dargarvic/tarea54/dgv25/Figura.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package es.gva.edu.dargarvic.tarea54.dgv25;
+
+/**
+ *
+ * @author Dario
+ */
+public interface Figura {
+    public double calcularArea();
+    public double calcularPerimetro();
+}
