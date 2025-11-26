289b80bc004b344009791102a3037d8433c5d739
Dario Garcia Vicente <dargarvic@alu.edu.gva.es>
2025-11-26 19:39:31 +0100

    Implementada clase triangulo.java Fix #4

---
+++ Tarea54-DGV25/src/main/java/es/gva/edu/dargarvic/tarea54/dgv25/Triangulo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.gva.edu.dargarvic.tarea54.dgv25;
+
+/**
+ *
+ * @author Dario
+ */
+public class Triangulo {
+    
+}
